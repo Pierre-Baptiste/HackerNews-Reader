507eddd49b0017b5fd99fbc229fe919ed25f74e6
Pierre-Baptiste DUPIRE <p.b.dupire@gmail.com>
2020-04-13 14:11:47 +0200

    rm newrelic

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require('newrelic');
-
 //server.js
 const express = require('express');
 const favicon = require('express-favicon');
@@ -13,11 +11,11 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function(req, res) {
+app.get('/ping', function (req, res) {
 	return res.send('pong');
 });
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
